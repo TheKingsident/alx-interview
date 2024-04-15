ab5494358180778272512d7da8bbd0c67073e300
TheKingsident <kingsident@gmail.com>
2024-04-15 02:56:38 +0100

    Updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,19 +3,20 @@
 Module that creates a Pascal triangle
 """
 
+
 def pascal_triangle(n):
-	"""
-	Function that creates a Pascal traingle
-	"""
-	pascal_triangle = []
+    """
+    Function that creates a Pascal traingle
+    """
+    pascal_triangle = []
 
-	for i in range(n):
-		row = [None for _ in range(i + 1)]
-		row [0], row [-1] = 1, 1
+    for i in range(n):
+        row = [None for _ in range(i + 1)]
+        row[0], row[-1] = 1, 1
 
-		for l in range(1, len(row) - 1):
-			row[l] = pascal_triangle[i - 1][l - 1] + pascal_triangle[i - 1][l]
+        for j in range(1, len(row) - 1):
+            row[j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j]
 
-		pascal_triangle.append(row)
+        pascal_triangle.append(row)
 
-	return (pascal_triangle)
+    return (pascal_triangle)
