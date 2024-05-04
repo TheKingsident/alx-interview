a0f874d36c669476f48d3771673f5db3a3593c12
TheKingsident <kingsident@gmail.com>
2024-05-04 21:26:28 +0100

    Updated logic

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,26 +14,26 @@ try:
     for line in sys.stdin:
         line = line.strip()
         match = re.match(regex, line)
-        
+
         if not match:
             continue
 
-    ip_address, date, status_code, file_size = match.groups()
+        ip_address, date, status_code, file_size = match.groups()
 
-    status_code = int(status_code)
-    file_size = int(file_size)
+        status_code = int(status_code)
+        file_size = int(file_size)
 
-    total_file_size += file_size
-    lines_count += 1
+        total_file_size += file_size
+        lines_count += 1
 
-    if status_code in [200, 301, 400, 404, 404, 405, 500]:
-        status_code_count[status_code] = status_code_count.get(
-            status_code, 0) + 1
-        
-    if lines_count % 10 == 0:
-        print("File size:", total_file_size)
-        for code in sorted(status_code_count.keys()):
-            print(f"{code}: {status_code_count[code]}")
+        if status_code in [200, 301, 400, 404, 404, 405, 500]:
+            status_code_count[status_code] = status_code_count.get(
+                status_code, 0) + 1
+
+        if lines_count % 10 == 0:
+            print("File size:", total_file_size)
+            for code in sorted(status_code_count.keys()):
+                print(f"{code}: {status_code_count[code]}")
 
 except KeyboardInterrupt:
     print("File size:", total_file_size)
