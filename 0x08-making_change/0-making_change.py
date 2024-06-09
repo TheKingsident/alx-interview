2127032d2870bf494ff9f3d6e771972f0d6ee1af
TheKingsident <kingsident@gmail.com>
2024-06-09 22:28:46 +0100

    Added files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" makeChange module
+"""
+
+
+def makeChange(coins, total):
+    """ determine the fewest number of coins needed to meet a
+        given amount total
+    """
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+This directory contains assignment files for the "0x08. Making Change" tasks from ALX.
